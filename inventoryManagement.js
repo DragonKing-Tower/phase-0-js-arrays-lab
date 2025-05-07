a5ed949976c5a1583ae2006cb50fa1090b347644
Cyril Rafalovich <replanttrees@gmail.com>
2025-05-07 11:49:54 -1000

    This is the first LAB for Module 5

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,6 +1,25 @@
 // Write your code here
+const products = ["Laptop","Phone","Headphones","Monitor"]
 
+//function to console log 1st item in products
+function logFirstProduct(){
+  console.log(products[0])
+}
 
+//Function to add new product
+function addProduct(porductName){
+  products.push(porductName)
+}
+
+//Function to change name of a product
+function updateProductName(position,newName){
+  products[position]=newName
+}
+
+//Function to remove final product
+function removeLastProduct(){
+  products.pop()
+}
 
 // Export the necessary parts for testing
 module.exports = {
